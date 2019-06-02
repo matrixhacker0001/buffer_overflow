5de644906cfaa057c5a6e1a6e512a6d0b8fc63c7
M4trix_H4ck3r <matrixhacker0001@gmail.com>
2019-06-02 22:24:37 +0530

    Create stack0.c

---
+++ protostar/stack0/stack0.c
@@ -0,0 +1,20 @@
+/*buffer overflow to override modified value*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    volatile int modified;
+    char buffer[64];
+
+    modified = 0;
+    gets(buffer);
+
+    if(modified != 0) {
+        printf("you have changed the 'modified' variable\n");
+    } else {
+        printf("Try again?\n");
+    }
+}
